8ad54fe3d2cc63a38c713b1a2d1c17d538b45191
Sara van den Bos <sara.vandenbos@iodigital.com>
2023-06-01 11:18:41 +0200

    fix(App): remove temporary button component in app.js for testing

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Button from './components/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
-      <Button 
-        text='Button' 
-        icon='arrow-right'
-        onPress={() => {console.log('wow u clicked blue')}} />
-      <Button 
-        text='Button' 
-        color='secondary'
-        onPress={() => {console.log('wow u clicked orange')}} />
     </View>
   );
 }
