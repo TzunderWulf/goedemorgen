a405900ab82d29db99fbb62097a28656b192add1
Sara van den Bos <sara.vandenbos@iodigital.com>
2023-06-05 00:42:39 +0200

    fix(InputField): added view to import list

--- components/InputField.js
+++ components/InputField.js
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
 export default function InputField({ label }) {
