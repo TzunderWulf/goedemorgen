318e69b9b0131f6b4c6cc17d2532840942e624a0
Sara van den Bos <sara.vandenbos@iodigital.com>
2023-06-05 00:24:57 +0200

    fix(HelpScreen): changed SplashScren to HelpScreen, since SplashScreen isn't needed

--- screens/HelpScreen.js
+++ screens/HelpScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 export default function SplashScreen() {
   return(
     <View>
-      <Text>Splashscreen</Text>
+      <Text>Helpscreen</Text>
     </View>
   );
 };
