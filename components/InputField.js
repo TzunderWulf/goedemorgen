60fcb000a11937aa765fdefa9a4c7d0640ace458
Sara van den Bos <sara.vandenbos@iodigital.com>
2023-06-05 00:41:21 +0200

    feat(InputField): created inputfield file + added basic code

---
+++ components/InputField.js
@@ -0,0 +1,14 @@
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+export default function InputField({ label }) {
+  return(
+    <View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
