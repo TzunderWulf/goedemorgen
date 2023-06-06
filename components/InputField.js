a7a055a134e55865d55ef2b0ff742f75146ff425
Sara van den Bos <sara.vandenbos@iodigital.com>
2023-06-06 23:26:07 +0200

    feat(InputField): created inputfield component

--- components/InputField.js
+++ components/InputField.js
@@ -1,14 +1,37 @@
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
-export default function InputField({ label }) {
+export default function InputField({ label, password }) {
   return(
-    <View>
-
+    <View style={styles.inputField}>
+      <Text style={styles.label}>{ label }</Text>
+      <TextInput
+        style={styles.input}
+        placeholder={label}
+        secureTextEntry={password} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  inputField: {
+    width: '100%',
+    marginVertical: 10,
+  },
+  input: {
+    width: '100%',
+    paddingVertical: 15,
+    paddingStart: 15,
+    fontSize: 20,
+    borderWidth: 3,
+    borderRadius: 5,
+    borderColor: '#233D4D',
+    backgroundColor: '#EFEFEF',
+    color: '#OEOEOE',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginBottom: 5,
+  },
 });
